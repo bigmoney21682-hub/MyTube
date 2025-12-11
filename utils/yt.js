eb297392a8bc0734a1962d281fa2f108339af702
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-11 10:39:27 -0500

    Update yt.js

--- utils/yt.js
+++ utils/yt.js
@@ -4,6 +4,7 @@ export function runYtDlp(url) {
   return new Promise((resolve, reject) => {
     exec(`yt-dlp -J "${url}"`, { maxBuffer: 1024 * 1024 * 50 }, (err, stdout) => {
       if (err) return reject(err);
+
       try {
         resolve(JSON.parse(stdout));
       } catch (e) {
